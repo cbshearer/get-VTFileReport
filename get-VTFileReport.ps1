80bc73ddf37175534f83cd42195c4a1744514f43
Chris Shearer <cbshearer@gmail.com>
2019-09-04 22:28:14 -0700

    Update get-VTFileReport.ps1

--- get-VTFileReport.ps1
+++ get-VTFileReport.ps1
@@ -3,11 +3,11 @@
 ## 9.4.2019
 ## VirusTotal Public API: https://developers.virustotal.com/reference#file-report
 
-## set TLS 1.2
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
 ## Get your own VT API key here: https://www.virustotal.com/gui/join-us
-    $VTApiKey = "1234567890abcdefedcba0987654321"
+    $VTApiKey = "xxxxx"
+
+## Set TLS 1.2
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Function submit-VTHash($VThash)
 {
